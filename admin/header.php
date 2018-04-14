ec0f5050e9c782fefe527c87d99a93f28728e89a
ashique23 <37500337+ashique23@users.noreply.github.com>
2018-04-15 00:46:26 +0600

    header

---
+++ admin/header.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+
+
+
+<div class="header-area col-md-12 text-center">
+		<div class="container">
+			<ul class="nav nav-pills text-center col-md-offset-5">
+    		<li><a href="welcome.php">Home</a></li>
+    		<li><a href="order.php">Order Request</a></li>
+    		<li><a href="paid.php">Paid Customer</a></li>
+            <li><a href="sub.php">Subscriver</a></li>
+    		<li><a href="carnew.php">Add New Car</a></li>
+            <li><a href="bid.php">Car BID </a></li>
+    		<li><a href="deletebid.php">Delete Car Item </a></li>
+    		<li><a href="logout.php">Logout</a></li>
+  		</ul>
+		</div>
+	</div>
