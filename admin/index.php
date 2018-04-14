0f18bce36f384b06c66dfcd6640df12a5e9c8cd7
tawsif001 <37498658+tawsif001@users.noreply.github.com>
2018-04-15 01:10:15 +0600

    landing page for users

---
+++ admin/index.php
@@ -0,0 +1,98 @@
+<?php
+	session_start();
+	if(isset($_SESSION['admin'])){
+	    header('location: welcome.php');
+	    
+	}
+	
+	if(isset($_POST['info_pass'])){
+		$name = $_POST['username'];
+		$pass = $_POST['pass'];
+		$error = array();
+
+		define('ADMIN','admin');
+		define('PASSWORD','123456');
+
+		if($name==NULL){
+			 $error['nm']="<h4>Usernmae Required</h4>";
+		}
+		if($pass==NULL){
+			 $error['pa']="<h4>Password required</h4>";
+		}
+		if($name!==ADMIN){
+			 $error['wr']="<h4>Wrong Username</h4>";
+		}
+		if($pass!==PASSWORD){
+			 $error['p']="<h4>Wrong Password</h4>";
+		}
+		
+		if(count($error)==0){
+		header('location: welcome.php');
+			$_SESSION['admin']= '';
+		}
+
+	}
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php		
+
+			include_once('title.php');
+
+			function title(){
+
+			// ... change title name...
+			echo $mytitle= "Admin Login";
+			
+		}
+
+	?>
+</head>
+<body>
+	
+</body>
+</html>
+<body>
+	<div class="admin-area col-md-12 col-xs-12 col-sm-12">
+		<div class="container">
+			<div class="form-area">
+					<h2>Admin Login | Car- Online Car Parking Website/h2>
+				<form action="" method="POST">
+					<input type="text" name="username" placeholder="Username">
+					<div class="error">
+						<?php 
+							if(isset($error['nm'])){
+								echo $error['nm'];
+							}
+							if(isset($error['wr'])){
+							    echo $error['wr'];
+							}
+						 ?>
+						 
+					</div>
+					<input type="password" name="pass" placeholder="Password">
+					<div class="error">
+						<?php 
+							if(isset($error['pa'])){
+								echo $error['pa'];
+							}
+						
+							if(isset($error['p'])){
+							    echo $error['p'];
+							}
+						 ?>
+					</div>
+					<input type="submit" value="Login" name="info_pass">
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
