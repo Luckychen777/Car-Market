8623456ed41475d6ab74161b8f466cf7c47543b9
HasnaHoqueMitu <30614707+HasnaHoqueMitu@users.noreply.github.com>
2018-04-15 00:18:37 +0600

    header and footer
    header and footer have been added and irrelevent pop-up fixed

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div class="footer-area">
+	<div class="container">
+		<footer>
+			<p>&copy; Copyright 2017 | All rights reserved by  Car Market</p>
+		</footer>
+	</div>
+</div>
---
+++ header.php
@@ -0,0 +1,29 @@
+<div class="header-area">
+	<div class="container">
+		<div class="row">
+			<div class="main-logo-area col-md-4 col-sm-4 col-xs-12">
+			<a href="index.php"><img class="img-responsive" src="images/logo.png" alt="Car website"></a>
+		</div>
+
+		<div class="menu-area col-md-8 col-sm-8 col-xs-12">
+			<div class="main-menu">
+				<ul class="nav-menu">
+
+					<li>
+						<form action="result.php" method='GET'>
+							<input class="my-form" type="email" name="semail" placeholder="Email" required="required">
+							<input type="submit" value="Track Order" name="search" class="btn-primary">
+						</form>
+					</li>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="index.php#about">About Us</a></li>
+					<li><a href="shop.php">Shop</a></li>
+					<li><a href="index.php#contact">Contact</a></li>
+					<li><a href="allcar.php">OldCar</a></li>
+					<li><a href="dashboard.php">Dashboard</a></li>
+				</ul>
+			</div>
+		</div>
+		</div>
+	</div>
+</div>
